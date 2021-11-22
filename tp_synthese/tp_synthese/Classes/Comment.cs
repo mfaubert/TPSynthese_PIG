0c10b3e39ff3b21505af618c2f126617349132a9
mfaubert <maite.faubert@gmail.com>
2021-11-22 11:30:47 -0500

    Comment Class

---
+++ tp_synthese/tp_synthese/Classes/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tp_synthese.Classes
+{
+    class Comment
+    {
+        public int Id;
+        public int UserId;
+        public string Texte;
+        public string ImageUrl;
+
+    }
+}
