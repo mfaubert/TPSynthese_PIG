868b44d012507554d2454ab1a17b7de4b1a00bae
MelkoBelrose <MelkoBelrose@gmail.com>
2021-12-08 15:17:12 -0500

    fusion

--- tp_synthese/tp_synthese/obj/Debug/netcoreapp3.1/AutreWindow.g.i.cs
+++ tp_synthese/tp_synthese/obj/Debug/netcoreapp3.1/AutreWindow.g.i.cs
@@ -87,13 +87,13 @@ namespace tp_synthese {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.8.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/tp_synthese;component/autrewindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/tp_synthese;V1.0.0.0;component/autrewindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\AutreWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -103,7 +103,7 @@ namespace tp_synthese {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.8.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
